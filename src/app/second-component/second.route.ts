69dc5e7a603f2880a5892282be4e1f6785951e0c
Momar NDIAYE <momar.ndiaye@intouchgroup.net>
2022-05-26 23:45:40 +0000

    lazy loading test

--- src/app/second-component/second.route.ts
+++ src/app/second-component/second.route.ts
@@ -1,7 +1,13 @@
+import { SecondComponentComponent } from "./second-component.component";
 import { TestComponent } from "./test/test.component";
 
 export const routesTest = [
   {
-    path:'test' , component : TestComponent
+    path:'' , component : SecondComponentComponent,
+    children : [
+      {
+        path: 'test' , component : TestComponent
+      }
+    ]
   }
 ]
